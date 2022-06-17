1b87409accf9fbace5700c27702dfa2bfc26b6af
1day1commit <heytaxitaxi@gmail.com>
2022-06-17 12:15:44 +0900

    6.17

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,31 +1,103 @@
-import img1 from "./img/img1.png";
-import img2 from "./img/img2.png";
-import img3 from "./img/img3.png";
+import product1 from "./img/product1.jpg";
+import product2 from "./img/product2.jpg";
+import product3 from "./img/product3.jpg";
+import product4a from "./img/product4a.jpg";
+import product4b from "./img/product4b.jpg";
+import product5a from "./img/product5a.jpg";
+import product5b from "./img/product5b.jpg";
+import product6 from "./img/product6.jpg";
+import product7 from "./img/product7.jpg";
+import product8 from "./img/product8.jpg";
+import product9 from "./img/product9.jpg";
+import product10 from "./img/product10.jpg";
 
 const product_card = [
   {
     id: 1,
-    product_name: "fCard",
-    description: "asasdsasss",
-    price: 250,
-    currency: "HKD",
-    thumb: [img1, img2, img3],
+    product_name: "Cuccio Cuticle Revitalizing Oil (Almond)",
+    description:
+      "75ml, 100% brand new & authentic. Made in USA. Provide moisture to hair and nail. Provide moisture to hair and nail ",
+    price: "150.00 HKD",
+    // currency: "HKD",
+    thumb: [product1],
   },
   {
     id: 2,
-    product_name: "faDard",
-    description: "sadfsff",
-    price: 30,
-    currency: "HKD",
-    thumb: "./img/icon 1.png",
+    product_name: "Cuccio Cuticle Revitalizing Oil Almond (Milk & Honey)",
+    description:
+      "75ml, 100% brand new & authentic. Made in USA. Provide moisture to hair and nail. Provide moisture to hair and nail",
+    price: "150.00 HKD",
+    // currency: "HKD",
+    thumb: [product2],
   },
   {
     id: 3,
-    product_name: "fasfdfddddd",
-    description: "sadfsff",
-    price: 450,
-    currency: "HKD",
-    thumb: "./img/product2a.jpg",
+    product_name: "Cuccio Cuticle Revitalizing Oil (White Limetta & Aloe Vera)",
+    description:
+      "75ml, 100% brand new & authentic. Made in USA. Provide moisture to hair and nail. Provide moisture to hair and nail",
+    price: "150.00 HKD",
+    // currency: "HKD",
+    thumb: [product3],
+  },
+  {
+    id: 4,
+    product_name: "Physiogel Calming Relief AI Lotion",
+    description:
+      "400ml, Contains skin lipids, including PEA, can quickly relieve dryness, redness and itching.Improve skin softness and smoothness",
+    price: "350.00 HKD",
+    // currency: "HKD",
+    thumb: [product4a, product4b],
+  },
+  {
+    id: 5,
+    product_name: "Physiogel Daily Moisture Therapy Cream",
+    description:
+      "150ml, provide long-lasting hydration for the skin, makes it less sensitive",
+    price: "250.00 HKD",
+    // currency: "HKD",
+    thumb: [product5a, product5b],
+  },
+  {
+    id: 6,
+    product_name: "Japanese dried Kelp",
+    description:
+      "135g, Made in Hokkaido, Japan. Effectively reduce blood pressure in your body",
+    price: "120.00 HKD",
+    // currency: "HKD",
+    thumb: [product6],
+  },
+  {
+    id: 7,
+    product_name: "Physiogel Calming Relief AI Cream",
+    description: "100ml, For dry, sensitive and eczema-prone skin",
+    price: "180.00 HKD",
+    // currency: "HKD",
+    thumb: [product7],
+  },
+  {
+    id: 8,
+    product_name: "Physiogel Daily Moisture Therapy Dermo-Cleanser",
+    description: "150ml, Soap-free, balance PH, reduce the skin stimulation",
+    price: "67.50 HKD",
+    // currency: "HKD",
+    thumb: [product8],
+  },
+  {
+    id: 9,
+    product_name: "Physiogel Calming Relief AI Lotion",
+    description:
+      "400ml, Contains skin lipids, including PEA, can quickly relieve dryness, redness and itching. Improve skin softness and smoothness",
+    price: "350.00 HKD",
+    // currency: "HKD",
+    thumb: [product9],
+  },
+  {
+    id: 10,
+    product_name: "Physiogel Calming Relief Face Cream Rich",
+    description: "40ml, Physiogel BioMimic Technology, good for allergic skin ",
+    price: "120.00 HKD",
+    // currency: "HKD",
+    thumb: [product10],
   },
 ];
 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,35 +7,39 @@ import SimpleImageSlider from "react-simple-image-slider";
 const Shop = () => {
   const listItems = product_card.map((item) => (
     <div className="card" key={item.id}>
-      <div className="card_header">
-        <div className="card_img">
+      <section className="card_container">
+        <div className="card_imgBox">
           <SimpleImageSlider
-            width={600}
-            height={500}
+            className="card_img"
+            width={400}
+            height={400}
             images={item.thumb}
             showBullets={true}
             showNavs={true}
           />
-          {/* <img src={require("" + item.thumb)} /> */}
+          <div className="card_info">
+            <h2>{item.product_name}</h2>
+            <p>{item.description}</p>
+            <h1 className="price">
+              {item.price}
+              <span>{item.currency}</span>
+            </h1>
+          </div>
         </div>
-        <h2>{item.product_name}</h2>
-        <p>{item.description}</p>
-        <p className="price">
-          {item.price}
-          <span>{item.currency}</span>
-        </p>
-      </div>
+      </section>
     </div>
   ));
   return (
-    <div className="shopAll">
+    <>
       <Navbar />
-      <div className="title">
-        <h2 className="shopTitle">Charter Rich's E-Shop</h2>
+      <div className="shopAll">
+        <div className="title">
+          <h2 className="shopTitle">Charter Rich's E-Shop</h2>
+        </div>
+        <div className="card-list">{listItems}</div>
       </div>
-      <div>{listItems}</div>
       <Footer />
-    </div>
+    </>
   );
 };
 
